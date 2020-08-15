b95c14548fbbed74c3bfcfac0e6a1799f7b815ed
deezone <darrendouglas.lee@gmail.com>
2020-08-14 23:04:43 -0400

    Issue-4 Added serial listening logic to loop() to support messages from server events

--- src/sensors/sensors.ino
+++ src/sensors/sensors.ino
@@ -7,28 +7,37 @@ uint8_t pin_led = 12;
  * setup()
  */
 void setup() {
-  Serial.begin(9600);
-  Serial.println("HydroBites Water Station (Sensors) serial connection started...");
-  delay(1000);
+  Serial.println("HydroBites Water Station");
+  Serial.println("v0.0.2");
+  Serial.println("");
 
-  Serial.println("HydroBites Water Station (Sensors <-> Server) serial connection started...");
+  Serial.begin(9600);
+  Serial.println("Sensors: serial connection started...");
+
+  Serial.println("Sensors: Sensors <-> Server serial connection started...");
   serverSerial.begin(9600);
   
-  Serial.println("HydroBites Water Station (Sensors) setup complete...");
+  Serial.println("Sensors: setup complete...");
+  Serial.flush();
 }
 
 /**
  * loop() - event loop
  */
 void loop() {
-  String incomingString="";
-  boolean stringReady = false;
-
-  // Send test "PONG"
-  serverSerial.println("Sensor PONG");
+  String serverMessage;
 
   while(serverSerial.available()) {
-    incomingString = serverSerial.readStringUntil('\n');
-    Serial.println((String)"from server: " + incomingString);
+    serverMessage = serverSerial.readStringUntil('\n');
+    Serial.println((String)"Server: " + serverMessage);
+
+    // Router for sensor actions
+    if (serverMessage.indexOf("GET /status") >= 0) {
+      // Send Serial message back to server to confirm receipt
+      serverSerial.println("serial_status: true");
+
+      // Log to serial monitor
+      Serial.println("serial_status: true");
+    }
   }
 }
